7eb8e9b027579c30a4a13c5382154de3102d79bd
mmd <91467840+jslee22@users.noreply.github.com>
2022-04-25 15:45:33 +0800

    Update btwz.py

--- configs/_base_/datasets/btwz.py
+++ configs/_base_/datasets/btwz.py
@@ -1,12 +1,12 @@
 # dataset settings
 dataset_type = 'CocoDataset'
-data_root = 'data2/coco/'
+data_root = 'data/coco/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
-    dict(type='Resize', img_scale=(600, 400), keep_ratio=True),
+    dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
     dict(type='Pad', size_divisor=32),
@@ -17,7 +17,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(600, 400),
+        img_scale=(1333, 800),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
